9a7512a83e1c5344ef8ab79794cfbb422c1d260f
Andre Loreth <andre.loreth@live.de>
2019-09-14 10:47:34 +0200

    chore: remove legacy packages

--- eslint/config/typescript.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'eslint-config-standard-with-typescript'
-  ],
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }]
-  }
-}
