51a947152102dc7b5de1765b7550b6f1bd829f9f
Andre Loreth <andre.loreth@live.de>
2019-09-14 18:58:44 +0200

    feat: update eslint-config-typescript

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -1,12 +1,11 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint-config-standard-with-typescript'
   ],
   rules: {
     '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
-    'brace-style': 'off',
-    /*'@typescript-eslint/brace-style': ['error', 'allman', { allowSingleLine: false }]*/
+    curly: ['error', 'multi', 'consistent']
   }
 }
